4607ba30921d4e8dc361c737e29d0082c52a8d68
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-06 19:00:24 +0800

    written the view test, (dont forget to call render)

--- spec/views/urls/index.html.erb_spec.rb
+++ spec/views/urls/index.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-# RSpec.describe "urls/index.html.erb", type: :view do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe "urls/index.html.erb", type: :view do
+  before :each do
+    FactoryGirl.create(:url)
+    @urls = Url.all
+  end
+
+  it "should list the long_url" do
+    render #!!!! this must be called!!!!
+    @urls.each do |url|
+      expect(rendered).to have_content(url.long_url)
+      expect(rendered).to have_content(url.short_url)
+    end
+  end
+
+
+
+end
