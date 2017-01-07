d5eecef5788eef24880939b24a4314fa1938c4b3
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-01-07 23:30:22 +0800

    new test in the interact_spec.rb

--- spec/feature/interact_spec.rb
+++ spec/feature/interact_spec.rb
@@ -10,6 +10,7 @@ describe "user clicks a link on the index page", :type => :feature do
   it "redirects to new" do
     visit root_path
     click_link "Short it"
+
     expect(current_path).to eq(new_url_path)
   end
 end
@@ -21,11 +22,23 @@ describe "user clicks the long_url", :type => :feature do
   end
 
   it "redirects to show page" do
-
     visit root_path
     click_link "www.google.com"
 
     expect(current_path).to eq(url_path(@url.id))
-  end
+    expect(page.body).to have_content(@url.long_url)
+    expect(page.body).to have_content(@url.short_url)
 
+  end
+end
+
+describe "user creates submit a form", :type => :feature do
+  it "redirects to index page with new entry" do
+    visit new_url_path
+    fill_in "Enter your URL", with: "www.verge.com"
+    click_button "Make it Short"
+
+    expect(current_path).to eq(urls_path)
+    expect(page.body).to have_content("www.verge.com")
+  end
 end
